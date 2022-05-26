b6e50b4a580f52bc6518ff706276d87f3cf20587
Mateus <felixmateus618@gmail.com>
2022-05-26 18:46:53 -0300

    aded responsividade.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
     
         <div className="text-zinc-900 flex items-center justify-between gap-[8px] bg-[#1D1C3B] rounded-lg p-[4px] m-0 m-auto mb-[8px] w-[100%] shadow-2xl">
             <div className="flex items-center gap-4 mobile:flex-col mobilemin:flex-col">
-              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/batman.svg" alt="image batman" />
+              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/batman.svg" alt="image Batman" />
               <div className="flex flex-col items-center text-zinc-100">
                 <span> Batman (2022) </span>
               </div>
@@ -40,7 +40,7 @@ export default function App() {
 
         <div className="text-zinc-900 flex items-center justify-between gap-[8px] bg-[#1D1C3B] rounded-lg p-[4px] m-0 m-auto mb-[8px] w-[100%] shadow-2xl mt-8">
             <div className="flex items-center gap-4 mobile:flex-col mobilemin:flex-col">
-              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/avatar.svg" alt="image avatar" />
+              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/avatar.svg" alt="image Avatar" />
               <div className="flex flex-col items-center text-zinc-100">
                 <span> Avatar (2009) </span>
               </div>
@@ -64,7 +64,7 @@ export default function App() {
 
         <div className="text-zinc-900 flex items-center justify-between gap-[8px] bg-[#1D1C3B] rounded-lg p-[4px] m-0 m-auto mb-[8px] w-[100%] shadow-2xl mt-8">
             <div className="flex items-center gap-4 mobile:flex-col mobilemin:flex-col">
-              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/vigadores.svg" alt="image vingadores era de ultron" />
+              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/vigadores.svg" alt="image Vingadores era de ultron" />
               <div className="flex flex-col items-center text-zinc-100">
                 <span> Vingadores Era de Ultron (2015) </span>
               </div>
@@ -88,7 +88,7 @@ export default function App() {
 
         <div className="text-zinc-900 flex items-center justify-between gap-[8px] bg-[#1D1C3B] rounded-lg p-[4px] m-0 m-auto mb-[8px] w-[100%] shadow-2xl mt-8">
             <div className="flex items-center gap-4 mobile:flex-col mobilemin:flex-col">
-              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/vigadoresUltimato.svg" alt="image vingadores ultimato" />
+              <img className="w-[92px] h-[92px] mr-4 rounded-lg mobile:w-[66px] mobile:h-[66px]" src="./src/image/vigadoresUltimato.svg" alt="image Vingadores ultimato" />
               <div className="flex flex-col items-center text-zinc-100">
                 <span> Vingadores ultimato (2019) </span>
               </div>
