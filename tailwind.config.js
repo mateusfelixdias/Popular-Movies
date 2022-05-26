4bbfb963ba8c87100887215ab5ef125a65fb319d
Mateus <felixmateus618@gmail.com>
2022-05-26 15:15:50 -0300

    config tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
       boxShadow: {
         '2xl': '10px 10px 20px rgba(0, 0, 0, 0.3)'
       }
-    },
-    screens: {
-      'mobile': {'min': '100px', 'max': '450px'}
-    },
+    }
   },
   plugins: [],
 }
