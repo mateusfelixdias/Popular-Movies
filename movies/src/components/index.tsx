0ffcba1a669650de013cebb7c492b5dc9b6383b7
Mateus <felixmateus618@gmail.com>
2022-06-08 11:54:59 -0300

    aded react router.

--- movies/src/components/index.tsx
+++ movies/src/components/index.tsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
+import { useShowOnlyFavorites } from "../../hooks/showOnlyFavorites";
 import { PopularMovies } from "./Steep/PopularMovies";
 import { SearchByMovie } from "./Steep/SearchByMovie";
 
 export function Movies() {
   const [movieName, setMovieName] = useState<string>("");
   const [userSearchedForMovie, setUserSearchedForMovie] = useState<boolean>(false);
+  const { showOnlyFavorites, setShowOnlyFavoritesOn, setShowOnlyFavoritesOff } = useShowOnlyFavorites();
 
   return (
     <main className="w-[100%] mobilemin:w-[550px] flex flex-col items-center">
-      <h1 className="text-4xl text-[#f1f5f9] pb-8 text-center">
+      <h1 className="text-4xl text-[#f1f5f9] pb-8 text-center italic">
         Filmes Populares
       </h1>
 
       <div className="bg-[#ffffff33] w-[50%] m-auto m-0 mb-[8px] flex p-[8px] rounded-lg">
         <input
-          className=" p-[8px] w-[100%] h-[100%] text-zinc-900 outline-none border-none bg-transparent"
+          className=" p-[8px] w-[100%] h-[100%] text-zinc-900 outline-none border-none bg-transparent italic text-zinc-100"
           type="text"
           name="pesquisar"
           placeholder="Digite algum filme para pesquisar..."
           onChange={(data) => {
             setMovieName(data.target.value);
+            setUserSearchedForMovie(false);
           }}
         />
         <button
@@ -29,12 +32,25 @@ export function Movies() {
             setUserSearchedForMovie(true);
           }}
         >
-          <img src="../../../image/pesquisar.svg" alt="pesquisar" />
+          <img
+            className="pr-2"
+            src="../../../image/pesquisar.svg"
+            alt="pesquisar"
+          />
         </button>
       </div>
 
       <div className="mt-8 text-zinc-200 text-center">
-        <input className="mr-[10px]" type="checkbox" name="filmsFavoritos" />
+        <input
+          className="mr-[10px]"
+          type="checkbox"
+          value={showOnlyFavorites}
+          onChange={() =>
+            showOnlyFavorites == "on"
+              ? setShowOnlyFavoritesOff()
+              : setShowOnlyFavoritesOn()
+          }
+        />
         <span>Mostrar apenas meus filmes favoritos.</span>
       </div>
 
