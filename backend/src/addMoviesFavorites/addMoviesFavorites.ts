4726e4e31e541a6e1b87e981ab3f076475a48284
Mateus <felixmateus618@gmail.com>
2022-06-09 15:18:35 -0300

    aded yarn

--- backend/src/addMoviesFavorites/addMoviesFavorites.ts
+++
@@ -1,9 +0,0 @@
-import { movieFavorite } from "../database/moviesFavorites";
-
-export async function addMoviesFavorites(req: any, res:any) {
-    const { favorite, movie } = req.body;
-
-    await movieFavorite.create({ favorite, movie }, error => {
-        return !error ? (res.status(200).send()): res.status(400).send();
-    });
-};
