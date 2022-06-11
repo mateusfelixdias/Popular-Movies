35e5b07b98bb28ffc90deb5bd4f6867c1dd281c9
Mateus <felixmateus618@gmail.com>
2022-06-11 14:56:04 -0300

    aded delete movie dos favorites.

---
+++ backend/src/delete/deleteMoviesFavorite.ts
@@ -0,0 +1,13 @@
+import { movieFavorite } from "../database/moviesFavorites";
+
+export async function deleteMoviesFavorites(req: any, res: any) {
+  const { title } = req.body;
+
+  const way = { way: { title } };
+
+  const deleteMovie = await movieFavorite.deleteOne(way);
+
+  if(!deleteMovie){
+    console.log(`No sucess.`)
+  };
+};
