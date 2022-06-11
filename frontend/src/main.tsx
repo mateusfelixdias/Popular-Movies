7e21e9b8f2e1684115c5758aebd5cd77546240ca
Mateus <felixmateus618@gmail.com>
2022-06-11 14:32:30 -0300

    modified route!

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,15 @@
-import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import "./global.css";
+import { Movies } from "./components";
 
 const roots = ReactDOM.createRoot(document.getElementById("root")!);
 
 roots.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/movies" element={<App/>}/>
+      <Route path="/" element={<Movies/>}/>
     </Routes>
   </BrowserRouter>
 );
