b85c1a28659df691ab4311b2fd2770fc0d32200b
Mateus <felixmateus618@gmail.com>
2022-06-11 09:09:07 -0300

    aded apiMethodsIsFavorite.

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
 });
+
+export const apiMoviesIsFavorites = axios.create({
+    baseURL: 'http://localhost:3003'
+});
