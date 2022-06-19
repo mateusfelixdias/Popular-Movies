366501ff08ecd8e999d1451a49a8ccb776fe5eab
Mateus <felixmateus618@gmail.com>
2022-06-19 14:03:28 -0300

    added component popular movies..

---
+++ frontend/src/components/Steeps/PopularMovies.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { api } from "../../../api/api";
+import { api_key } from "../../../api/chaveApi";
+import { PatternMovies } from "./PatternMovies";
+
+export function PopularMovies() {
+  const [resultsPopularMovies, setResultsPopularMovies] = useState([]);
+  const [showPopularMovies, setShowPopularMovies] = useState<boolean>(false);
+
+  async function getPopularMoveis() {
+    const { results } = (await api.get(`movie/popular?api_key=${api_key}&language=pt-BR&page=1`)).data;
+    setResultsPopularMovies(results);
+    setShowPopularMovies(true);
+  };
+
+  useEffect(() => {
+    getPopularMoveis();
+  }, []);
+
+  return (
+    <div>
+      {showPopularMovies
+        ? resultsPopularMovies.map((movie: any) => {
+            return (
+              <PatternMovies
+                alt={`Imagem do(a) ${movie.title}.`}
+                description={movie.overview}
+                image={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                rating={movie.vote_average}
+                title={movie.title}
+              />
+            );
+          })
+        : null}
+    </div>
+  );
+};
