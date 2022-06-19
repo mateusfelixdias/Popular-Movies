b27d1575196e6c6dcff46371b0bdd79150ff3ea7
Mateus <felixmateus618@gmail.com>
2022-06-19 13:59:58 -0300

    added component movies favorites.

---
+++ frontend/src/components/Steeps/MoviesFavorites.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { getToYouFavoriteslocalStorage } from "../../saving-movies-favorites-localstorage/getToYouFavoritesLocalStorage";
+import { PatternMovies } from "./PatternMovies";
+
+export function MoviesFavorites() {
+  const [allMoviesSelectedAsFavorites, setAllMoviesSelectedAsFavorites] = useState([]);
+  const [notHaveMoviesAsFavorites, setNotHaveMoviesAsFavorites] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getAllMoviesSelectedAsFavorites = getToYouFavoriteslocalStorage();
+
+    if (getAllMoviesSelectedAsFavorites?.length === 0) {
+      window.alert(`No Momento Você Não Selecionou Nenhum Filme Como Favorito!`);
+      setNotHaveMoviesAsFavorites(false);
+    } else {
+      setNotHaveMoviesAsFavorites(true);
+      setAllMoviesSelectedAsFavorites(getAllMoviesSelectedAsFavorites);
+    };
+  }, []);
+
+  return (
+    <>
+      {notHaveMoviesAsFavorites
+        ? allMoviesSelectedAsFavorites.map((movie: any) => {
+            return (
+              <PatternMovies
+                alt={movie.alt}
+                description={movie.description}
+                image={movie.image}
+                rating={movie.rating}
+                title={movie.title}
+              />
+            );
+          })
+        : null}
+    </>
+  );
+};
