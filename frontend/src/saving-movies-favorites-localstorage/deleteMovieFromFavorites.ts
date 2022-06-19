6ae1f9533b647c5111a158640069e56055516e38
Mateus <felixmateus618@gmail.com>
2022-06-19 14:32:10 -0300

    added delete movies from favorites.

--- frontend/src/saving-movies-favorites-localstorage/deleteMovieFromFavorites.ts
+++ frontend/src/saving-movies-favorites-localstorage/deleteMovieFromFavorites.ts
@@ -2,12 +2,10 @@ import { getToYouFavoriteslocalStorage } from "./getToYouFavoritesLocalStorage";
 
 export function deleteMovieFromFavorites(alt: string) {
   const pathToAllFavoriteMovies = "all favorite movies";
-
   const allFavoriteMovies = getToYouFavoriteslocalStorage() || [];
 
   const movieIsNotFavorite = allFavoriteMovies.find((movie: any) => movie?.alt === alt);
 
   const onlyFavoriteMovies = allFavoriteMovies.filter((movie: any) => movie?.alt != movieIsNotFavorite.alt);
-
   localStorage.setItem(pathToAllFavoriteMovies,JSON.stringify(onlyFavoriteMovies));
 };
