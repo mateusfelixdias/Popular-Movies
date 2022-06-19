abb61b58fdd572bf7f39bd14dff869fee461cd87
Mateus <felixmateus618@gmail.com>
2022-06-19 13:51:09 -0300

    added movies salvos no localstorage.

---
+++ frontend/src/saving-movies-favorites-localstorage/getToYouFavoritesLocalStorage.ts
@@ -0,0 +1,7 @@
+export function getToYouFavoriteslocalStorage() {
+  const allMoviesSelectedAsFavorites = localStorage.getItem("all favorite movies");
+
+  if (allMoviesSelectedAsFavorites) {
+    return JSON.parse(allMoviesSelectedAsFavorites);
+  };
+};
