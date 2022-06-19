fe3dd7dabd8bd4df20daaa06e8727f9c661b707c
Mateus <felixmateus618@gmail.com>
2022-06-19 13:50:07 -0300

    added movies favorites in localstorage.

---
+++ frontend/src/saving-movies-favorites-localstorage/addedFavoriteMoviesInLocalstorage.ts
@@ -0,0 +1,24 @@
+import { getToYouFavoriteslocalStorage } from "./getToYouFavoritesLocalStorage";
+
+export async function addedFavoriteMoviesInLocalstorage(
+  alt: string,
+  description: string,
+  image: string,
+  rating: string,
+  title: string
+) {
+  const pathToAllFavoriteMovies = "all favorite movies";
+  
+  const allFavoriteMovies =  getToYouFavoriteslocalStorage();
+
+  const addingMovieToFavorites = allFavoriteMovies || [];
+  addingMovieToFavorites.push({ alt, description, image, rating, title });
+
+  const addingMovieToFavoritesJson = JSON.stringify(addingMovieToFavorites);
+  localStorage.setItem(pathToAllFavoriteMovies, addingMovieToFavoritesJson);
+};
+
+
+export function selectingMoviesWithTrueOrFalseInLocalStorage(title: string, isFavorite: string) {
+  localStorage.setItem(title, String(isFavorite));
+};
